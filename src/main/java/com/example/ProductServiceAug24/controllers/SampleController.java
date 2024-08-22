71c73f670b3f2fc0d2ef63883a000d14c568540f
Ajay Patil <patilajay7521@gmail.com>
2024-08-22 22:31:11 +0530

    Helo World Controller

---
+++ src/main/java/com/example/ProductServiceAug24/controllers/SampleController.java
@@ -0,0 +1,24 @@
+package com.example.ProductServiceAug24.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String helloworld(){
+        return "Hello World";
+    }
+
+    //Endpoint name
+    @GetMapping("/hello/{name}")
+    public String helloWithName(@PathVariable("name") String name ){
+        return "Hello " + name;
+    }
+    @GetMapping("/show/{showId}/seat/{seatId}")
+    public String bmsExample(@PathVariable("showId") String showId,@PathVariable("seatId") int seatId ){
+        return "Hello "+ showId+" "+seatId;
+    }
+}
